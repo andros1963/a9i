e2c5c4eee07344b2d4c11e7519765a7efa30fbd5
andros <andros@SilentWorker>
2017-01-11 17:13:03 +0100

    init

---
+++ pelicanconf.py
@@ -0,0 +1,140 @@
+#!/usr/bin/env python
+#!/usr/bin/env python
+#!/usr/bin/env python
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+# -*- coding: utf-8 -*- #
+# -*- coding: utf-8 -*- #
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+from __future__ import unicode_literals
+from __future__ import unicode_literals
+from __future__ import unicode_literals
+
+
+
+
+AUTHOR = u'Andros'
+AUTHOR = u'Andros'
+AUTHOR = u'Andros'
+AUTHOR = u'Andros'
+SITENAME = u'modificabile'
+SITENAME = u'modificabile'
+#SITENAME = u'modificabile'
+SITENAME = u'modificabile'
+SITEURL = ''
+#SITEURL = ''
+SITEURL = ''
+SITEURL = ''
+
+
+
+
+PATH = 'content'
+PATH = 'content'
+PATH = 'content'
+PATH = 'content'
+
+
+
+
+TIMEZONE = 'Europe/Paris'
+TIMEZONE = 'Europe/Paris'
+TIMEZONE = 'Europe/Paris'
+TIMEZONE = 'Europe/Paris'
+
+
+
+
+DEFAULT_LANG = u'it'
+DEFAULT_LANG = u'it'
+DEFAULT_LANG = u'it'
+DEFAULT_LANG = u'it'
+
+
+
+
+# Feed generation is usually not desired when developing
+# Feed generation is usually not desired when developing
+# Feed generation is usually not desired when developing
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+FEED_ALL_ATOM = None
+FEED_ALL_ATOM = None
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+CATEGORY_FEED_ATOM = None
+CATEGORY_FEED_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+AUTHOR_FEED_RSS = None
+AUTHOR_FEED_RSS = None
+AUTHOR_FEED_RSS = None
+
+
+
+
+# Blogroll
+# Blogroll
+# Blogroll
+# Blogroll
+LINKS = (('Pelican', 'http://getpelican.com/'),
+LINKS = (('Pelican', 'http://getpelican.com/'),
+LINKS = (('Pelican', 'http://getpelican.com/'),
+LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('Python.org', 'http://python.org/'),
+         ('Python.org', 'http://python.org/'),
+         ('Python.org', 'http://python.org/'),
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),
+         ('You can modify those links in your config file', '#'),)
+         ('You can modify those links in your config file', '#'),)
+         ('You can modify those links in your config file', '#'),)
+         ('You can modify those links in your config file', '#'),)
+
+
+
+
+# Social widget
+# Social widget
+# Social widget
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+SOCIAL = (('You can add links in your config file', '#'),
+SOCIAL = (('You can add links in your config file', '#'),
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+          ('Another social link', '#'),)
+          ('Another social link', '#'),)
+          ('Another social link', '#'),)
+
+
+
+
+DEFAULT_PAGINATION = 10
+DEFAULT_PAGINATION = 10
+DEFAULT_PAGINATION = 10
+DEFAULT_PAGINATION = 10
+
+
+
+
+# Uncomment following line if you want document-relative URLs when developing
+# Uncomment following line if you want document-relative URLs when developing
+# Uncomment following line if you want document-relative URLs when developing
+# Uncomment following line if you want document-relative URLs when developing
+RELATIVE_URLS = True
+RELATIVE_URLS = True
+RELATIVE_URLS = True
+RELATIVE_URLS = True
